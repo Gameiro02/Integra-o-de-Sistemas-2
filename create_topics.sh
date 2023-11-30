3a7d203ae6c2bf4017fdd69ff6262f85535c4bc5
Gameiro <gmeiro2002@gmail.com>
2023-11-30 18:04:22 +0000

    topic creator

---
+++ create_topics.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Defina a lista de tópicos que você deseja criar
+TOPICOS=("SockPurchasesTopic" "SockSalesTopic" "ResultsTopic" "DBInfoTopic")
+
+# Loop para criar os tópicos
+for topico in "${TOPICOS[@]}"; do
+  kafka_2.13-3.6.0/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic "$topico"
+  echo "Tópico '$topico' criado com sucesso."
+done
---
+++ demo/src/main/java/com/is3/Streams.java
@@ -0,0 +1,5 @@
+package com.is3;
+
+public class Streams {
+
+}
