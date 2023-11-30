85ae4306c2caf7c878a20fdf06e7d379b760ded0
Gameiro <gmeiro2002@gmail.com>
2023-11-30 23:42:02 +0000

    req 5

--- demo/src/main/java/com/is3/KafkaStreams/StreamsApp.java
+++ demo/src/main/java/com/is3/KafkaStreams/StreamsApp.java
@@ -1,18 +1,33 @@
 package com.is3.KafkaStreams;
 
 import java.util.Properties;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+import com.is3.model.Sale;
+import com.is3.util.LocalDateTimeAdapter;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.KStream;
+import java.time.LocalDateTime;
 
 public class StreamsApp {
+    private final Gson gson;
+
+    public StreamsApp() {
+        gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create();
+    }
 
     public static void main(String[] args) {
+        StreamsApp app = new StreamsApp();
+        app.startStream();
+    }
+
+    public void startStream() {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG, "sock-shop-streams");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "broker1:9092");
@@ -22,23 +37,30 @@ public class StreamsApp {
         StreamsBuilder builder = new StreamsBuilder();
         KStream<String, String> source = builder.stream("SockSalesTopic");
 
-        source.peek((key, value) -> System.out.println("Received message. Key: " + key + " Value: " + value))
+        source.mapValues(this::processSale)
                 .to("ResultsTopic");
 
-        // // Process the stream as required
-        // // For now, just forwarding the messages as they are
-        // source.to("Results");
-
         KafkaStreams streams = new KafkaStreams(builder.build(), props);
-
-        streams.setStateListener(
-                (newState, oldState) -> System.out.println("State changed from " + oldState + " to " + newState));
-
         streams.start();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             System.out.println("Shutting down streams");
             streams.close();
         }));
+    }
+
+    private String processSale(String value) {
+        try {
+            Sale sale = gson.fromJson(value, Sale.class);
+            double profit = calculateProfit(sale);
+            return String.valueOf(profit); // Retorna o lucro como String
+        } catch (JsonSyntaxException e) {
+            e.printStackTrace();
+            return "Error";
+        }
+    }
+
+    private double calculateProfit(Sale sale) {
+        return sale.getSale_price() * sale.getQuantity_sold(); // Lucro = sale_price * quantity_sold
     }
 }
