379c3a85b9e210395c59e5d4921a431553c0ed15
Gameiro <gmeiro2002@gmail.com>
2023-11-30 17:40:47 +0000

    first

---
+++ demo/src/model/Purchase.java
@@ -0,0 +1,54 @@
+package com.hood.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
+
+import java.time.LocalDateTime;
+
+import com.hood.util.LocalDateTimeAdapter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Purchase {
+    @Getter @Setter
+    private int purchase_id;
+
+    @Getter @Setter
+    private int sock_id;
+
+    @Getter @Setter
+    private int quantity;
+
+    @Getter
+    private double price;
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        this.price = price;
+    }
+
+    @Getter @Setter
+    private LocalDateTime purchase_date;
+
+    public String toJson() {
+        try {
+            Gson gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create();
+            return gson.toJson(this);
+        } catch (JsonSyntaxException e) {
+            System.err.println("Error serializing object to JSON: " + e.getMessage());
+            return null;
+        }
+    }
+}
---
+++ demo/src/model/Sock.java
@@ -0,0 +1,47 @@
+package com.hood.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sock {
+    @Getter @Setter
+    private int sock_id;
+
+    @Getter @Setter
+    private SockType type;
+
+    @Getter
+    private double price;
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Price cannot be negative");
+        }
+        this.price = price;
+    }
+
+    @Getter @Setter
+    private int quantity_available;
+
+    @Getter @Setter
+    private int supplier_id;
+
+    public String toJson() {
+        try {
+            Gson gson = new Gson();
+            return gson.toJson(this);
+        } catch (JsonSyntaxException e) {
+            System.err.println("Error serializing object to JSON: " + e.getMessage());
+            return null;
+        }
+    }
+}
